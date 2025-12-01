e24c1920d80bc99bdc43d6e7930650bdacb7e30d
eiweiw321 <eiweiw321@gmail.com>
2025-12-01 21:49:26 +0700

    add a java file

---
+++ Documents/NetBeansProjects/Lab01/src/lab01/exercise.java
@@ -0,0 +1,20 @@
+package lab01;
+import java.util.*;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author eiwei
+ */
+public class exercise {
+    
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your exercise time 1 : ");
+        int MyNum = sc.nextInt();
+     
+     
+} }
