d7177505c0db6bfd8503165d842ce1254693850d
eiweiw321 <eiweiw321@gmail.com>
2025-12-06 18:33:46 +0700

    modify and move a file to a folder

--- Documents/NetBeansProjects/Lab01/src/lab01/exercise.java
+++ Documents/NetBeansProjects/Lab01/src/lab01/exercise.java
@@ -1,4 +1,5 @@
 package lab01;
+
 import java.util.*;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -14,7 +15,15 @@ public class exercise {
     public static void main (String [] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your exercise time 1 : ");
-        int MyNum = sc.nextInt();
+        int myNum_1 = sc.nextInt();
+        System.out.print("Enter your exercise time 2 : ");
+        int myNum_2 = sc.nextInt();
+        int result = myNum_1 + myNum_2;
+        int miniute = result / 60;
+        int hour = miniute / 3600;
+        int sec = result % 60;
+        System.out.print("It is " + hour +  " hours " + miniute + " minutes " + sec + " seconds. ");
+        
      
      
 } }
