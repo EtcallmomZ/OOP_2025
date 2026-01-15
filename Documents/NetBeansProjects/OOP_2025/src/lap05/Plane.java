02338d09df1bbfddf393853d1d180d90f3835b39
eiweiw321 <eiweiw321@gmail.com>
2026-01-16 03:54:16 +0700

    add lab05

---
+++ Documents/NetBeansProjects/OOP_2025/src/lap05/Plane.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lap05;
+
+/**
+ *
+ * @author eiwei
+ */
+public class Plane extends Vehicle {
+    public void setPlaneInfo(int s, String t){
+        this.setFuel(s);
+        this.setTopSpeed(t);
+    }
+    public void fly(){
+        if (this.getFuel() < 200) {
+            System.out.println("Please add fuel.");
+        }else{
+            System.out.println("Fly.");
+            this.setFuel(this.getFuel() - 200);
+        }
+    }
+    public void showPlaneInfo(){
+        System.out.print("Plane detail is, ");
+        this.showInfo();
+    }
+    
+}
