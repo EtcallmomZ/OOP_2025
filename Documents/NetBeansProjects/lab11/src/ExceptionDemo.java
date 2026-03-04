35126bf6f6b3b56e7f8d152b135b5163d7ffa2e0
alien programmer <eiweiw321@gmail.com>
2026-03-04 19:00:31 +0700

    add lab11

---
+++ Documents/NetBeansProjects/lab11/src/ExceptionDemo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author eiwei
+ */
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        try{
+            double a = Double.parseDouble(args[0]);
+            
+        }catch{
+        }
+        
+        
+    }
+    
+}
